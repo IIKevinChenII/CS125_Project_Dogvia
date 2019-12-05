e4b13ff1103c6c18eef8a6ada16faed76c52d244
IIKevinChenII <yuc3@illinois.edu>
2019-12-05 00:34:33 -0600

    Changed UI design

--- app/src/main/java/com/example/cs125finalproject/WrongActivity.java
+++ app/src/main/java/com/example/cs125finalproject/WrongActivity.java
@@ -2,9 +2,7 @@ package com.example.cs125finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class WrongActivity extends AppCompatActivity {
